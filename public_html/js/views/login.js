83ffe8d1f8a215c124b17458e3afc3a07bc9d3f1
mid135 <moskalukdima@mail.ru>
2014-12-06 16:14:03 +0300

    merge done

--- public_html/js/views/login.js
+++ public_html/js/views/login.js
@@ -1,28 +1,103 @@
 define([
     'backbone',
-    'tmpl/login'
-    ],
-    function(
-        Backbone,
-        tmpl
-    )
-{
+    'tmpl/login',
+    'models/session'
+], function(
+    Backbone,
+    tmpl,
+    sessionModel
+){
+
     var View = Backbone.View.extend({
-
+        el: $('.login'),
         template: tmpl,
-        initialize: function () {
-            this.render();
-        },
-        render: function () {
-            this.$el.html(this.template);
-        },
-        show: function () {
-            // TODO
-        },
-        hide: function () {
-            // TODO
-        }
+        session: sessionModel,
 
+        events: {
+            "submit form[name=login-form]": "authClick",
+            "click input[name=login]": "loginClick",
+            "click input[name=passw]": "passwordClick",
+            "blur input[name=login]": "loginBlur",
+            "blur input[name=passw]": "passwordBlur"
+        },
+
+        initialize: function () {
+            this.listenTo(this.session, 'successAuth', this.loginSuccess);
+            this.listenTo(this.session, 'errorAuth', this.loginError);
+            this.render();
+            this.$el.hide();
+        },
+
+        render: function () {
+            this.$el.html(this.template());
+        },
+
+        show: function () {
+            this.trigger('reshow', this);
+        },
+
+        authClick: function(event) {
+            event.preventDefault();
+            var username = this.$("input[name=login]").val(),
+                password = this.$("input[name=passw]").val();
+
+            var btnSubmit = this.$("input[name=submit]");
+            btnSubmit.prop('disabled', true).addClass("form__footer__button_disabled").delay(1700).queue(
+                function(next) {
+                    $(this).attr('disabled', false);
+                    $(this).removeClass("form__footer__button_disabled");
+                    next();
+                }
+            );
+
+            if (this.validate(username, password)) {
+                var url = this.$('.form').data('action');
+
+                this.session.postAuth(url, {
+                    login: username,
+                    passw: password
+                });
+            }
+        },
+
+        loginSuccess: function(data) {
+            this.trigger('success');
+        },
+
+        loginError: function(message) {
+            this.showError(message, ".login-error");
+        },
+
+        showError: function(message, div_error) {
+            var elem = this.$(div_error).slideDown().delay(3000).slideUp();
+            elem.html("<p>" + message + "</p>");
+        },
+
+        validate: function(username, password) {
+            var status= true;
+            if (username == '') {
+                status = false;
+                this.showError("Missing username", ".login-error");
+            }
+            if (password == '') {
+                status = false;
+                this.showError("Missing password", ".passw-error");
+            }
+            return status;
+        },
+
+        loginClick: function() {
+            this.$(".user-icon").css("left","-48px");
+        },
+        passwordClick: function() {
+            this.$(".pass-icon").css("left","-48px");
+        },
+        loginBlur: function() {
+            this.$(".user-icon").css("left","0px");
+        },
+        passwordBlur: function() {
+            this.$(".pass-icon").css("left","0px");
+        }
     });
 
     return new View();
--- public_html/js/views/main.js
+++ public_html/js/views/main.js
@@ -1,28 +1,61 @@
 define([
     'backbone',
-    'tmpl/main'
-    ],
-    function(
-        Backbone,
-        tmpl
-    )
-{
+    'tmpl/main',
+    'models/session'
+], function(
+    Backbone,
+    tmpl,
+    sessionModel
+){
+
     var View = Backbone.View.extend({
-
+        el: $('.main'),
         template: tmpl,
+        session: sessionModel,
+        events: {
+            "click .logout": "logout"
+        },
         initialize: function () {
+            this.listenTo(this.session, 'main:anonymous', this.userNotIdentified);
+            this.listenTo(this.session, 'main:known', this.userIdentified);
+            this.listenTo(this.session, 'successLogout', this.logoutSuccess);
+            this.listenTo(this.session, 'errorLogout', this.logoutError);
             this.render();
+            this.$el.hide();
         },
-        render: function () {
-            this.$el.html(this.template);
-        },
-        show: function () {
-            // TODO
-        },
-        hide: function () {
-            // TODO
-        }
 
+        render: function () {
+            this.$el.html(this.template());
+        },
+
+        show: function () {
+            this.session.postLogin('main');
+        },
+        logout: function (event) {
+            event.preventDefault();
+            this.session.postLogout();
+        },
+        
+        userIdentified: function(data) {
+            this.$(".auth, .reg").hide();
+            this.$(".to_profile, .exit").show();
+            this.trigger('reshow', this);
+        },
+        userNotIdentified: function() {
+            this.$(".to_profile, .exit").hide();
+            this.$(".auth, .reg").show();
+            this.trigger('reshow', this);
+        },
+
+        logoutSuccess: function (data) {
+            this.trigger('success');
+            this.show();
+        },
+
+        logoutError: function (message) {
+            var elem = this.$(".alert-error").slideDown().delay(3000).slideUp();
+            elem.html("<p>" + message + "</p>");
+        }
     });
 
     return new View();
